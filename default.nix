4a9b72be4daa701d32d017482f6743a7a5116a41
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2018-06-20 16:38:14 +0200

    Started using the validity overlay from the validity repo

--- default.nix
+++ default.nix
@@ -1,4 +1,11 @@
-import (import ./nixpkgs.nix) {
-  overlays = [ (import ./overlay.nix) ];
+let
+  pkgsv = import (import ./nixpkgs.nix);
+  pkgs = pkgsv {};
+  validity-overlay = import (
+    (pkgs.fetchFromGitHub (import ./validity-version.nix)
+    + "/overlay.nix")
+  );
+in pkgsv {
+  overlays = [ validity-overlay (import ./overlay.nix) ];
   config.allowUnfree = true;
 }
---
+++ validity-version.nix
@@ -0,0 +1,6 @@
+{
+  owner = "NorfairKing";
+  repo = "validity";
+  rev = "d1dc976d107d4ce854daa5cb6e6997f587779dc1";
+  sha256 = "0grbyr7l6967wry7h6cvd22bwm7qfrgaq10fidj6zfk2mwicjcvf";
+}
