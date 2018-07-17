3407a33f9a49b17d47ef04fc6a757ea350a29a05
Niklas Hambüchen <mail@nh2.me>
2018-07-17 17:50:59 +0200

    nix: Work around hpack files not being used when cabal files exist

--- default.nix
+++ default.nix
@@ -5,7 +5,27 @@ let
     (pkgs.fetchFromGitHub (import ./validity-version.nix)
     + "/overlay.nix")
   );
+  # TODO: Remove when https://github.com/NixOS/cabal2nix/pull/360 is merged and available
+  cabal2nix-overlay = final: previous:
+    with final.haskell.lib; {
+      haskellPackages = previous.haskellPackages.override (old: {
+        overrides = final.lib.composeExtensions (old.overrides or (_: _: {})) (
+
+          self: super: {
+            cabal2nix = overrideCabal super.cabal2nix (old: {
+              src = pkgs.fetchFromGitHub {
+                owner = "nh2";
+                repo = "cabal2nix";
+                rev = "5721bed2a598a018119413bfe868bd286735cb15";
+                sha256 = "1436ri6nlfcgd263byb596dcx6g4l9fx47hm11vfh34x849r2kcy";
+              };
+            });
+
+          }
+        );
+      });
+    };
 in pkgsv {
-  overlays = [ validity-overlay (import ./overlay.nix) ];
+  overlays = [ cabal2nix-overlay validity-overlay (import ./overlay.nix) ];
   config.allowUnfree = true;
 }
